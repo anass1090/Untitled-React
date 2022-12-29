4604e3003fcf2c7e954e0a3ad9f3f1360d4ff247
Anoos <55783196+anass1090@users.noreply.github.com>
2022-12-29 15:48:00 +0100

    Commit from TeleportHQ

---
+++ src/views/desktop1.js
@@ -0,0 +1,336 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './desktop1.css'
+
+const Desktop1 = (props) => {
+  return (
+    <div className="desktop1-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="desktop1-desktop1">
+        <div className="desktop1-paneel-top">
+          <img
+            alt="Rectangle119"
+            src="/playground_assets/rectangle119-epg-200h.png"
+            className="desktop1-rectangle1"
+          />
+          <span className="desktop1-text">
+            <span>Control Panel - “naam van je account”</span>
+          </span>
+        </div>
+        <button className="desktop1-socialbackbutton">
+          <span className="desktop1-text02">
+            <span>
+              <span>Social Media</span>
+              <br></br>
+              <span></span>
+            </span>
+          </span>
+          <div className="desktop1-arrow">
+            <img
+              alt="Line3I153"
+              src="/playground_assets/line3i153-e6ct.svg"
+              className="desktop1-line3"
+            />
+            <img
+              alt="Line5I153"
+              src="/playground_assets/line5i153-eby9.svg"
+              className="desktop1-line5"
+            />
+          </div>
+        </button>
+        <div className="desktop1-sugguesties">
+          <img
+            alt="Rectangle5133"
+            src="/playground_assets/rectangle5133-b70c-200h.png"
+            className="desktop1-rectangle5"
+          />
+          <div className="desktop1-suggesties">
+            <button className="desktop1-add-button">
+              <img
+                alt="Rectangle6I152"
+                src="/playground_assets/rectangle6i152-nn5g.svg"
+                className="desktop1-rectangle6"
+              />
+              <span className="desktop1-text07">
+                <span>Toevoegen</span>
+              </span>
+            </button>
+            <img
+              alt="Line6I152"
+              src="/playground_assets/line6i152-ddqa.svg"
+              className="desktop1-line6"
+            />
+            <img
+              alt="Vector1I152"
+              src="/playground_assets/vector1i152-x7j.svg"
+              className="desktop1-vector1"
+            />
+            <span className="desktop1-text09">Social Media bedrijven...</span>
+            <span className="desktop1-text10">
+              <span>Kies of zoek een Social Media bedrijf</span>
+            </span>
+          </div>
+        </div>
+        <div className="desktop1-andere-informatie">
+          <img
+            alt="Rectangle2111"
+            src="/playground_assets/rectangle2111-uzsk-500w.png"
+            className="desktop1-rectangle2"
+          />
+          <img
+            alt="Line1110"
+            src="/playground_assets/line1110-xhxi.svg"
+            className="desktop1-line1"
+          />
+          <button className="desktop1-websitebutton">
+            <a
+              href="jouwwebsite.nl"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="desktop1-link"
+            >
+              <img
+                alt="Rectangle4I152"
+                src="/playground_assets/rectangle4i152-ca55.svg"
+                className="desktop1-rectangle4"
+              />
+            </a>
+            <div className="desktop1-bekijk-website-n-l">
+              <span className="desktop1-text12">
+                <span>Bekijk Website (NL)</span>
+              </span>
+            </div>
+          </button>
+          <div className="desktop1-andere-informatie1">
+            <span className="desktop1-text14">
+              <span>Website</span>
+            </span>
+            <div className="desktop1-andereinformatie2">
+              <span className="desktop1-text16">
+                <span>Mijn bedrijf</span>
+              </span>
+              <span className="desktop1-text18">
+                <span>Webshop</span>
+              </span>
+              <span className="desktop1-text20">
+                <span>Nieuws</span>
+              </span>
+              <span className="desktop1-text22">
+                <span>Aanbieding</span>
+              </span>
+              <span className="desktop1-text24">
+                <span>Nieuwsbrief</span>
+              </span>
+              <img
+                alt="Star1I152"
+                src="/playground_assets/star1i152-p7hr.svg"
+                className="desktop1-star1"
+              />
+              <img
+                alt="Star2I152"
+                src="/playground_assets/star2i152-40ap.svg"
+                className="desktop1-star2"
+              />
+              <img
+                alt="Star3I152"
+                src="/playground_assets/star3i152-xyvm.svg"
+                className="desktop1-star3"
+              />
+              <img
+                alt="Star4I152"
+                src="/playground_assets/star4i152-ycf.svg"
+                className="desktop1-star4"
+              />
+              <img
+                alt="Star5I152"
+                src="/playground_assets/star5i152-svtf.svg"
+                className="desktop1-star5"
+              />
+              <img
+                alt="Star6I152"
+                src="/playground_assets/star6i152-bnjt.svg"
+                className="desktop1-star6"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="desktop1-facebook">
+          <div className="desktop1-icon">
+            <img
+              alt="Rectangle6I153"
+              src="/playground_assets/rectangle6i153-ys6ij.svg"
+              className="desktop1-rectangle61"
+            />
+            <span className="desktop1-text26">
+              <span>
+                <span>Zoek logo of laat leeg voor standaard...</span>
+                <br></br>
+                <span></span>
+              </span>
+            </span>
+          </div>
+          <div className="desktop1-facebook1">
+            <div className="desktop1-facebook2">
+              <span className="desktop1-text31"></span>
+              <img
+                alt="Rectangle15I153"
+                src="/playground_assets/rectangle15i153-50lb-200h.png"
+                className="desktop1-rectangle15"
+              />
+              <img
+                alt="Rectangle16I153"
+                src="/playground_assets/rectangle16i153-fsu-200h.png"
+                className="desktop1-rectangle16"
+              />
+              <span className="desktop1-text32">
+                <span>publiceren</span>
+              </span>
+              <span className="desktop1-text34">
+                <span>Plaats hier de link naar je pagina...</span>
+              </span>
+              <img
+                alt="RedTrashSimpleIcon6I153"
+                src="/playground_assets/redtrashsimpleicon6i153-rzdr-200h.png"
+                className="desktop1-red-trash-simple-icon6"
+              />
+              <img
+                alt="Rectangle17I153"
+                src="/playground_assets/rectangle17i153-c4gd-200h.png"
+                className="desktop1-rectangle17"
+              />
+              <img
+                alt="Rectangle20I153"
+                src="/playground_assets/rectangle20i153-yq9a-200w.png"
+                className="desktop1-rectangle20"
+              />
+            </div>
+            <img
+              alt="arrowupdownI153"
+              src="/playground_assets/arrowupdowni153-crzf.svg"
+              className="desktop1-arrowupdown"
+            />
+          </div>
+          <img
+            alt="image27334"
+            src="/playground_assets/image27334-8ud9-200h.png"
+            className="desktop1-image2"
+          />
+        </div>
+        <div className="desktop1-twitter">
+          <div className="desktop1-twitter1">
+            <img
+              alt="Rectangle11I152"
+              src="/playground_assets/rectangle11i152-rhat-200h.png"
+              className="desktop1-rectangle11"
+            />
+            <img
+              alt="Rectangle12I152"
+              src="/playground_assets/rectangle12i152-bw8c-200h.png"
+              className="desktop1-rectangle12"
+            />
+            <span className="desktop1-text36">
+              <span>publiceren</span>
+            </span>
+            <span className="desktop1-text38">
+              <span>Plaats hier de link naar je pagina...</span>
+            </span>
+            <img
+              alt="RedTrashSimpleIcon4I152"
+              src="/playground_assets/redtrashsimpleicon4i152-8f3s-200h.png"
+              className="desktop1-red-trash-simple-icon4"
+            />
+            <div className="desktop1-icon1">
+              <img
+                alt="Rectangle6I152"
+                src="/playground_assets/rectangle6i152-3ty7.svg"
+                className="desktop1-rectangle62"
+              />
+              <span className="desktop1-text40">
+                <span>
+                  <span>Zoek logo of laat leeg voor standaard...</span>
+                  <br></br>
+                  <span></span>
+                </span>
+              </span>
+            </div>
+            <img
+              alt="Rectangle18I152"
+              src="/playground_assets/rectangle18i152-rt39-200h.png"
+              className="desktop1-rectangle18"
+            />
+          </div>
+          <img
+            alt="arrowupdown1522"
+            src="/playground_assets/arrowupdown1522-xvsq.svg"
+            className="desktop1-arrowupdown1"
+          />
+          <img
+            alt="image37335"
+            src="/playground_assets/image37335-c099-200h.png"
+            className="desktop1-image3"
+          />
+        </div>
+        <div className="desktop1-instagram">
+          <div className="desktop1-instagram1">
+            <img
+              alt="Rectangle9I152"
+              src="/playground_assets/rectangle9i152-8u1s-200h.png"
+              className="desktop1-rectangle9"
+            />
+            <img
+              alt="Rectangle10I152"
+              src="/playground_assets/rectangle10i152-6d3n-200h.png"
+              className="desktop1-rectangle10"
+            />
+            <span className="desktop1-text45">
+              <span>publiceren</span>
+            </span>
+            <img
+              alt="RedTrashSimpleIcon2I152"
+              src="/playground_assets/redtrashsimpleicon2i152-pzvg-200h.png"
+              className="desktop1-red-trash-simple-icon2"
+            />
+            <span className="desktop1-text47">
+              <span>Plaats hier de link naar je pagina...</span>
+            </span>
+            <div className="desktop1-icon2">
+              <img
+                alt="Rectangle6I152"
+                src="/playground_assets/rectangle6i152-mbf.svg"
+                className="desktop1-rectangle63"
+              />
+              <span className="desktop1-text49">
+                <span>
+                  <span>Zoek logo of laat leeg voor standaard...</span>
+                  <br></br>
+                  <span></span>
+                </span>
+              </span>
+            </div>
+            <img
+              alt="Rectangle19I152"
+              src="/playground_assets/rectangle19i152-m8bn-200h.png"
+              className="desktop1-rectangle19"
+            />
+          </div>
+          <img
+            alt="arrowupdown1521"
+            src="/playground_assets/arrowupdown1521-ypq9.svg"
+            className="desktop1-arrowupdown2"
+          />
+          <img
+            alt="Schermafbeelding2022101311525117334"
+            src="/playground_assets/schermafbeelding2022101311525117334-lqqi-200h.png"
+            className="desktop1-schermafbeelding202210131152511"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Desktop1
